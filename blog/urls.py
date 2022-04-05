6f11a94cc589caf604fdfac7e1eda356fe136d3b
Marlon-Peralta <91367587+Marlon-Peralta@users.noreply.github.com>
2022-04-05 02:47:33 -0500

    final

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import BlogListView, BlogCreateView
+from .views import BlogDeleteView, BlogDetailView, BlogListView, BlogCreateView, BlogUpdateView
 
 app_name = "blog"
 
@@ -7,4 +7,10 @@ urlpatterns = [
     path('', BlogListView.as_view(), name = "home"),
 
     path('create/', BlogCreateView.as_view(), name = "create"),
+
+    path('<int:pk>/', BlogDetailView.as_view(), name = "detail"),
+
+    path('<int:pk>/update/', BlogUpdateView.as_view(), name = "update"),
+
+    path('<int:pk>/delete/', BlogDeleteView.as_view(), name = "delete"),
 ]
