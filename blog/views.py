73af282eac1c3c9195bb4be21492e5f94807b370
Marlon-Peralta <91367587+Marlon-Peralta@users.noreply.github.com>
2022-04-05 00:28:05 -0500

    Creamos forms

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render, redirect
 from django.views.generic import View
+from .forms import PostCreateForms
+from .models import Post
 
 # Create your views here.
 class BlogListView(View):
     def get(self, request, *args, **kwargs):
         context = {
-
         }
         return render(request, 'blog_list.html', context)
+
+class BlogCreateView(View):
+    def get(self, request, *args, **kwargs):
+        form = PostCreateForms()
+        context = {
+            'form':form
+        }
+        return render(request, 'blog_create.html', context)
+    
+    def post(self, request, *args, **kwargs):
+        if request.method == "POST":
+            form = PostCreateForms(request.POST)
+            if form.is_valid():
+                title = form.cleaned_data.get('title')
+                content = form.cleaned_data.get('content')
+
+                p, created = Post.objects.get_or_create(title = title, content = content)
+                p.save()
+                return redirect('blog:home')
+
+
+
+        context = {
+        }
+        return render(request, 'blog_create.html', context)
